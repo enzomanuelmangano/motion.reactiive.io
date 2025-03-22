94c0dcaf09c0c66706923057bd634fe0954db389
Enzo Manuel Mangano <enzomanuelmangano@gmail.com>
2025-03-22 16:45:45 +0100

    chore: add quick support for all fonts

--- App.tsx
+++ App.tsx
@@ -1,4 +1,28 @@
+import { useFonts } from 'expo-font';
+
 import { App } from './src';
 
+const AppContainer = () => {
+  const [fontsLoaded] = useFonts({
+    'SF-Pro-Rounded-Bold': require('./assets/fonts/SF-Pro-Rounded-Bold.otf'),
+    'SF-Pro-Rounded-Heavy': require('./assets/fonts/SF-Pro-Rounded-Heavy.otf'),
+    'SF-Compact-Rounded-Medium': require('./assets/fonts/SF-Compact-Rounded-Medium.otf'),
+    regular: require('./assets/fonts/regular.ttf'),
+    outfit: require('./assets/fonts/outfit.ttf'),
+    bold: require('./assets/fonts/bold.ttf'),
+    'AddingtonCF-Light': require('./assets/fonts/AddingtonCF-Light.otf'),
+    'FiraCode-Regular': require('./assets/fonts/FiraCode-Regular.ttf'),
+    FiraCodeMedium: require('./assets/fonts/FiraCode-Medium.ttf'),
+    'Honk-Regular': require('./assets/fonts/honk-regular.otf'),
+    'Honk-Bold': require('./assets/fonts/honk-bold.otf'),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return <App />;
+};
+
 // eslint-disable-next-line import/no-default-export
-export default App;
+export default AppContainer;
