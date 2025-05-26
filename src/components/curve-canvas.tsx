4313c00b56aad6c49252af943180564bffabb900
Enzo Manuel Mangano <enzomanuelmangano@gmail.com>
2025-05-26 15:15:33 +0200

    refactor: enhance CurveCanvas component with gesture handling and animated styles for improved interactivity and performance

--- src/components/curve-canvas.tsx
+++ src/components/curve-canvas.tsx
@@ -1,15 +1,20 @@
 import { Canvas } from '@shopify/react-native-skia';
 import { useWindowDimensions } from 'react-native';
 import type { SharedValue } from 'react-native-reanimated';
-import {
+import Animated, {
   cancelAnimation,
   Easing,
   useAnimatedReaction,
   useSharedValue,
   withSpring,
   withTiming,
+  interpolateColor,
+  useAnimatedStyle,
+  interpolate,
 } from 'react-native-reanimated';
 import { useStyles } from 'react-native-unistyles';
+import { Gesture, GestureDetector } from 'react-native-gesture-handler';
+import Color from 'color';
 
 import { SpringCurve } from './spring-curve';
 import { BezierCurve } from './bezier-curve';
@@ -43,6 +48,29 @@ export const CurveCanvas = ({
 
   const bezierProgress = useSharedValue(0);
   const springProgress = useSharedValue(0);
+  const isHovered = useSharedValue(0);
+
+  const startAnimation = () => {
+    cancelAnimation(bezierProgress);
+    bezierProgress.value = 0;
+    bezierProgress.value = withTiming(1, {
+      duration: bezierParams.duration.value,
+      easing: Easing.bezier(
+        bezierParams.x1.value,
+        bezierParams.y1.value,
+        bezierParams.x2.value,
+        bezierParams.y2.value,
+      ),
+    });
+
+    cancelAnimation(springProgress);
+    springProgress.value = 0;
+    springProgress.value = withSpring(1, {
+      mass: springParams.mass.value,
+      damping: springParams.damping.value,
+      stiffness: springParams.stiffness.value,
+    });
+  };
 
   useAnimatedReaction(
     () => {
@@ -51,64 +79,93 @@ export const CurveCanvas = ({
         spring: springParams,
       };
     },
-    ({ bezier, spring }) => {
-      cancelAnimation(bezierProgress);
-      bezierProgress.value = 0;
-      bezierProgress.value = withTiming(1, {
-        duration: bezier.duration.value,
-        easing: Easing.bezier(
-          bezier.x1.value,
-          bezier.y1.value,
-          bezier.x2.value,
-          bezier.y2.value,
-        ),
-      });
-
-      cancelAnimation(springProgress);
-      springProgress.value = 0;
-      springProgress.value = withSpring(1, {
-        mass: spring.mass.value,
-        damping: spring.damping.value,
-        stiffness: spring.stiffness.value,
-      });
+    () => {
+      startAnimation();
     },
   );
 
+  const tapGesture = Gesture.Tap().onEnd(() => {
+    startAnimation();
+  });
+
+  const hoverGesture = Gesture.Hover()
+    .onBegin(() => {
+      isHovered.value = withSpring(1, {
+        mass: 0.5,
+      });
+    })
+    .onEnd(() => {
+      isHovered.value = withSpring(0);
+    });
+
+  const composedGesture = Gesture.Simultaneous(tapGesture, hoverGesture);
+
+  const basePrimary = 'rgba(255,255,255,0.0)';
+  const baseSecondary = 'rgba(255,255,255,0.03)';
+
+  const baseBorder = theme.colors.border.primary;
+  const baseBorderSecondary = Color(baseBorder).lighten(1).toString();
+
+  const animatedStyle = useAnimatedStyle(() => {
+    const backgroundColor = interpolateColor(
+      isHovered.value,
+      [0, 1],
+      [basePrimary, baseSecondary],
+    );
+
+    return {
+      width: canvasWidth,
+      height: canvasHeight,
+      backgroundColor,
+      borderRadius: 12,
+      borderWidth: theme.strokeWidths.thin,
+      borderColor: interpolateColor(
+        isHovered.value,
+        [0, 1],
+        [baseBorder, baseBorderSecondary],
+      ),
+    };
+  });
+
+  const rCanvasStyle = useAnimatedStyle(() => {
+    return {
+      flex: 1,
+      transform: [{ scale: interpolate(isHovered.value, [0, 1], [1, 0.98]) }],
+    };
+  });
   return (
-    <Canvas
-      style={{
-        width: canvasWidth,
-        height: canvasHeight,
-        backgroundColor: theme.colors.background.secondary,
-        borderRadius: 12,
-        borderWidth: theme.strokeWidths.thin,
-        borderColor: theme.colors.border.primary,
-      }}>
-      <BezierCurve
-        progress={bezierProgress}
-        width={canvasWidth}
-        height={canvasHeight}
-        x1={bezierParams.x1}
-        y1={bezierParams.y1}
-        x2={bezierParams.x2}
-        y2={bezierParams.y2}
-        color={theme.colors.primary.bezier}
-        strokeWidth={theme.strokeWidths.medium}
-        horizontalPadding={theme.spacing.xxl}
-        verticalPadding={theme.spacing.xxl}
-      />
-      <SpringCurve
-        progress={springProgress}
-        width={canvasWidth}
-        height={canvasHeight}
-        mass={springParams.mass}
-        damping={springParams.damping}
-        stiffness={springParams.stiffness}
-        horizontalPadding={theme.spacing.xxl}
-        verticalPadding={theme.spacing.xxl}
-        color={theme.colors.primary.spring}
-        strokeWidth={theme.strokeWidths.medium}
-      />
-    </Canvas>
+    <GestureDetector gesture={composedGesture}>
+      <Animated.View style={animatedStyle}>
+        <Animated.View style={rCanvasStyle}>
+          <Canvas style={{ flex: 1 }}>
+            <BezierCurve
+              progress={bezierProgress}
+              width={canvasWidth}
+              height={canvasHeight}
+              x1={bezierParams.x1}
+              y1={bezierParams.y1}
+              x2={bezierParams.x2}
+              y2={bezierParams.y2}
+              color={theme.colors.primary.bezier}
+              strokeWidth={theme.strokeWidths.medium}
+              horizontalPadding={theme.spacing.xxl}
+              verticalPadding={theme.spacing.xxl}
+            />
+            <SpringCurve
+              progress={springProgress}
+              width={canvasWidth}
+              height={canvasHeight}
+              mass={springParams.mass}
+              damping={springParams.damping}
+              stiffness={springParams.stiffness}
+              horizontalPadding={theme.spacing.xxl}
+              verticalPadding={theme.spacing.xxl}
+              color={theme.colors.primary.spring}
+              strokeWidth={theme.strokeWidths.medium}
+            />
+          </Canvas>
+        </Animated.View>
+      </Animated.View>
+    </GestureDetector>
   );
 };
